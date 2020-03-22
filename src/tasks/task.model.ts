f158775fd1b4b8cc70b48a6e7e6d11d0b804fa0c
Alex Tran <alex.tran1502@gmail.com>
2020-03-22 02:16:50 -0500

    Added tasks module

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,13 @@
+
+export interface Task {
+    id: string,
+    title: string,
+    description: string,
+    status: TaskStatus
+}
+
+export enum TaskStatus {
+    OPEN = 'OPEN',
+    IN_PROGESS = 'IN_PROGRESS',
+    DONE = 'DONE'
+}
