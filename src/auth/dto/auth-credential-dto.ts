a11f7224fdc46254b4344f8d4d0132cd9e6b7249
Alex Tran <alex.tran1502@gmail.com>
2020-03-23 23:24:59 -0500

    string password enformence

--- src/auth/dto/auth-credential-dto.ts
+++ src/auth/dto/auth-credential-dto.ts
@@ -1,9 +1,18 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, MinLength, MaxLength, Matches } from "class-validator";
 
 export class AuthCredentialDto {
     @IsNotEmpty()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
 
+    
     @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(
+        /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        {message: 'password is weak'}
+    )
     password: string;
 }
